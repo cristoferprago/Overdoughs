9483b24117fecb58f8d1c2f48e6a4916b95d74be
cristoferprago <cristoferprago0904@gmail.com>
2022-09-22 11:23:39 +0800

    Update global.js

--- assets/global.js
+++ assets/global.js
@@ -934,10 +934,42 @@ $(document).ready(function() {
 })
   
 
-$(document).ready(function(){
-  $('.menubar-slider').each(function(){
-    var num_slides = 5;
-
-    $(this).slick({slidesToShow:num_slides});
-  });
-});
+$(document).ready(function() {
+  $(".menubar-slider").each(function (){
+    
+    var num_slides = $(this).data("slides");
+  
+  $(this).slick({slidesToShow:4,slidesToScroll: 4,
+    responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+                
+                
+                });
+  })
+})
